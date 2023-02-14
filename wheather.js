04a79b4f1e2db19084c1b003dcafe9752fb07e8a
IliaKuryndin <6apsyk@mail.ru>
2023-02-14 10:00:44 +0300

    подготовка структуры проекта

---
+++ wheather.js
@@ -0,0 +1,17 @@
+import { getArgs } from "./helpers/args.js"
+import { printHelp } from "./services/log.service.js"
+
+const initCLI = () => {
+    const args = getArgs(process.argv)
+    console.log(args)
+
+    if (args.h) {
+        return printHelp()
+    }
+    if (args.s) {
+
+    }
+    if (args.t) { }
+}
+
+initCLI()
